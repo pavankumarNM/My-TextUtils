16fbe8df8321c4868e924170eabbe9692a035a51
pavankumarNM <pavannm2001@gmail.com>
2023-04-08 11:01:13 +0530

    attached

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,37 @@
 import './App.css';
-import About from './components/About';
-import Alerts from './components/Alerts';
+//import About from './components/About';
+//import Alerts from './components/Alerts';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
 
 
 function App() {
   return (
     <>
-    <Router>
+    {/* <Router> */}
     <Navbar title ="Textutils" about="about textutils"/>
     <div className='container'>
-    <Switch>
+    {/* <Switch>
           <Route path="/about">
             <About />
           </Route>
           <Route path="/">
              <Textform heading=' Enter the Text Below to Analyze'/> 
           </Route>
-     </Switch>
-    
+     </Switch> */}
+       <Textform heading=' Enter the Text Below to Analyze'/> 
+
     </div>
-    </Router>
+   {/* </> </Router> */}
+
     </>
   );
 }
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <Link className="navbar-brand" to="/">{props.title}</Link>
+  <a className="navbar-brand" href="#">{props.title}</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -13,11 +13,11 @@ export default function Navbar(props) {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
-        <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
       </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/about">{props.about}</Link>
-      </li>
+      {/* <li className="nav-item">
+        <a className="nav-link" href="/about">{props.about}</a>
+      </li> */}
       
       
     </ul>
